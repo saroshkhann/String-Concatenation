46ed82594f13d52c9067453c00bd1f846f640578
saroshkhann <ksarosh785@gmail.com>
2026-03-28 17:11:18 +0500

    Swapping

---
+++ swapping of variable.py
@@ -0,0 +1,16 @@
+a = 5
+b = 6
+
+print("BEFORE SWAPPING")
+
+print(f"\nValue of Variable a:{a}")
+print(f"Value of Variable b:{b}")
+
+temp = a
+a = b
+b = temp
+
+print("\nAFTER SWAPPING")
+
+print(f"\nValue of Variable a:{a}")
+print(f"Value of Variable b:{b}")
